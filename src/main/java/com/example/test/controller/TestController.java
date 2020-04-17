aa5f50ca941fe5fb27bedcbfb8b33724ee7470cd
Roman <rom2517pop@gmail.com>
2020-04-17 02:50:59 +0200

    added test controller, view, and 'Transactional' in service

---
+++ src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,50 @@
+package com.example.test.controller;
+
+import com.example.test.model.TestEntity;
+import com.example.test.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController("/")
+public class TestController {
+
+    @Autowired
+    private TestService service;
+
+    @GetMapping
+    public ModelAndView showPage() {
+        return new ModelAndView("index");
+    }
+
+    @PostMapping
+    public ModelAndView addEntity(@RequestParam String firstName,
+                                  @RequestParam String lastName) {
+        ModelAndView mav = new ModelAndView("index");
+
+        TestEntity entity = new TestEntity();
+        entity.setFirstName(firstName);
+        entity.setLsatName(lastName);
+
+        mav.addObject("entity", service.save(entity));
+        return mav;
+    }
+
+    @DeleteMapping
+    public ModelAndView deleteEntity(@RequestParam Integer id) {
+        service.delete(id);
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/all")
+    public ModelAndView showAll(@RequestParam Integer id) {
+        ModelAndView mav = new ModelAndView("index");
+        List<TestEntity> list = service.getAll();
+        mav.addObject("all", mav);
+        return mav;
+    }
+
+
+}
--- src/main/java/com/example/test/service/TestService.java
+++ src/main/java/com/example/test/service/TestService.java
@@ -1,4 +1,36 @@
 package com.example.test.service;
 
+import com.example.test.dao.TestRepository;
+import com.example.test.model.TestEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class TestService {
+
+    @Autowired
+    private TestRepository repository;
+
+    @Transactional
+    public TestEntity save(TestEntity entity) {
+        return repository.save(entity);
+    }
+
+    @Transactional
+    public TestEntity getOne(Integer id) {
+        return repository.getOne(id);
+    }
+
+    @Transactional
+    public List<TestEntity> getAll() {
+        return repository.findAll();
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
 }

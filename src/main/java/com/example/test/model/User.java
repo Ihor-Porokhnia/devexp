3401a4079ab6fee1f1cf883a3f2cad2dc3ba7031
Roman <rom2517pop@gmail.com>
2020-04-22 03:26:07 +0200

    create entities user and role

---
+++ src/main/java/com/example/test/model/User.java
@@ -0,0 +1,36 @@
+package com.example.test.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.Set;
+
+@Data
+@Table(name = "usr")
+public class User {
+
+    @Column(name = "login")
+    private String login;
+
+    @Column
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Email
+    private String email;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    private Set<Role> roles;
+
+}

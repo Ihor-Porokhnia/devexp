d3b7cd2e7fd11463f753a62a2fc7374b75215914
Roman <rom2517pop@gmail.com>
2020-04-22 03:30:11 +0200

    update test and user services

---
+++ src/main/java/com/example/test/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.test.service;
+
+import com.example.test.dao.UserRepository;
+import com.example.test.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements ParentService<User> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void delete(User user) {
+        userRepository.delete(user);
+    }
+
+    public User findByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
+}

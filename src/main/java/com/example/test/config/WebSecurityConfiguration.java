d3b4ccf9a98afc275657eb564ff3b04c5fa211a8
Roman <rom2517pop@gmail.com>
2020-04-22 06:24:26 +0200

    repared authentication

--- src/main/java/com/example/test/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/test/config/WebSecurityConfiguration.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -40,6 +42,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(NoOpPasswordEncoder.getInstance())
                 .usersByUsernameQuery("select username, password, active from usr where username=?")
                 .authoritiesByUsernameQuery
-                        ("select u.username, ur.roles from usr u inner join user-role ur on u.id=ur.user_id where u.username=?");
+                        ("select u.username, ur.roles from usr u inner join user_role ur on u.id=ur.user_id where u.username=?");
     }
 }
